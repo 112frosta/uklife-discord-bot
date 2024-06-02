f63f7fb595bf9498ac482a8d1fa1fbf4e34f6df2
112frosta <112frosta@proton.me>
2024-06-02 17:40:14 +0200

    initialize command handler on bot ready event

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import "dotenv/config";
+
 import { Client } from "discord.js";
+import { initializeCommands } from "./handlers/commandHandler";
 
 const client = new Client({
   intents: ["GuildMembers", "GuildMessages"],
 });
 
-client.on("ready", () => {
+client.on("ready", async () => {
+  await initializeCommands(client);
   console.log(`Logged in as ${client.user?.tag}`);
 });
 
