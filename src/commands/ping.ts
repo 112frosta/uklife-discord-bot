e23a90f628bbc8dd82b409fb4bcb7bc35cd33602
112frosta <112frosta@proton.me>
2024-06-03 23:08:07 +0200

    Test for option command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -2,7 +2,13 @@ import { SlashCommandBuilder } from "discord.js";
 
 export const config = new SlashCommandBuilder()
   .setName("ping")
-  .setDescription("Replies with ping!");
+  .setDescription("Replies with ping!")
+  .addStringOption((option) =>
+    option
+      .setName("input")
+      .setDescription("The input to echo back")
+      .setRequired(true)
+  );
 
 export const execute: CommandExecute = async (interaction) => {
   await interaction.reply("Pong!");
