da9316a49fb112163fe8e99c5e988677e43d55c9
112frosta <112frosta@proton.me>
2024-06-03 23:08:20 +0200

    Added global CacheCommandInteraction Type

--- global.d.ts
+++ global.d.ts
@@ -2,4 +2,5 @@ import { CommandInteraction } from "discord.js";
 
 declare global {
   type CommandExecute = (interaction: CommandInteraction) => void;
+  type CacheCommandInteraction = CommandInteraction<"cached">;
 }
